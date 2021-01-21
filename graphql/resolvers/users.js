f1668e799ad36b014856a0d501b19d00efbf3792
Dion Pettaway <dionp@fullsail.edu>
2021-01-20 20:58:06 -0500

    I'm dumb I didn't compare the user info in DB to the submitted user info. Uniqueness check is now working correctly

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -7,16 +7,16 @@ module.exports = {
     Mutation: {
         async register(_, {registerInput: {username, email, password, confirmPassword}}) {
             // TODO: User validation
-            
+
             // TODO: User and email are unique
             const _user = await User.findOne({$or:[{username}, {email}]});
-            if (_user.username) {
+            if (_user.username === username) {
                 throw new UserInputError('Username is taken', {
                     errors: {
                         username: 'Username is currently in use, please try another.'
                     }
                 });
-            } else if (_user.email) {
+            } else if (_user.email === email) {
                 throw new UserInputError('Email is already in use', {
                     errors: {
                         email: "Email is already in use, please use another email."
