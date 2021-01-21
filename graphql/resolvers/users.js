85439960e72831bad024f63ef5843fc62d6468db
Dion Pettaway <dionp@fullsail.edu>
2021-01-20 20:44:15 -0500

    Making sure username or email is unique on GraphQL layer

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,12 +1,28 @@
 const User = require('../../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const {UserInputError} = require('apollo-server')
 
 module.exports = {
     Mutation: {
-        async register(_, {registerInput: {username, email, password, confirmPassword}}, context, info) {
+        async register(_, {registerInput: {username, email, password, confirmPassword}}) {
             // TODO: User validation
+            
             // TODO: User and email are unique
+            const _user = await User.findOne({$or:[{username}, {email}]});
+            if (_user.username) {
+                throw new UserInputError('Username is taken', {
+                    errors: {
+                        username: 'Username is currently in use, please try another.'
+                    }
+                });
+            } else if (_user.email) {
+                throw new UserInputError('Email is already in use', {
+                    errors: {
+                        email: "Email is already in use, please use another email."
+                    }
+                });
+            }
             // TODO: Salt and hash password with auth token
             password = await bcrypt.hash(password, 14);
             const newUser = new User({
