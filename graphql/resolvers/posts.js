0c8cde0bf8cd2ccae539363aaa234bb82c704ce1
Dion Pettaway <dionp@fullsail.edu>
2021-01-21 19:10:54 -0500

    misc commit

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,3 +1,5 @@
+const { AuthenticationError } = require('apollo-server');
+const { CLOSING } = require('ws');
 const Post = require('../../models/Post');
 const auth = require('../../utils/auth')
 
@@ -25,20 +27,40 @@ module.exports = {
             }
         }
     },
+    
     Mutation: {
         async createPost(_, {body}, context) {
             const user = auth(context);
-            console.log(user);
+            // console.log(user);
 
-            const newPost = new Post({
+            const _post = new Post({
                 body,
                 user: user.id,
                 username: user.username,
                 createdAt: new Date().toISOString()
             });
 
-            const post = await newPost.save();
+            const post = await _post.save();
             return post;
+        },
+        async deletePost(_, {postId}, context) {
+            const user = auth(context);
+            const _post = await Post.findById(postId);
+
+            try {
+                console.log(user.id, typeof(user.id));
+                console.log(typeof(_post.user));
+                if (user.id == _post.user) {
+                    await _post.delete();
+                    return 'Post has been successfully deleted'
+                } else {
+                    throw new AuthenticationError('Action not allowed');
+                }
+
+            } catch (err) {
+                throw new Error(err)
+            }
+
         }
     }
 }
