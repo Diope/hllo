fb9d17a204f30b892a98f52916a50e54c8b37f73
Dion Pettaway <dionp@fullsail.edu>
2021-02-15 17:56:56 -0500

    Sigh, still didn't switch to a new branch jfc what is wrong with me

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -2,9 +2,17 @@ import React from 'react';
 import {Card, Label, Icon} from 'semantic-ui-react';
 
 export default function PostCard({post}) {
+    const { body, createdAt, id, username, likeCount, likes, commentCOunt} = post
     return (
-        <div>
-            <h1>{post.username}</h1>
-        </div>
+        <Card>
+            <Card.Content>
+                <Card.Header>{username}</Card.Header>
+                <Card.Meta>{createdAt}</Card.Meta>
+                <Card.Description>{body}</Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+                <p>buttons</p>
+            </Card.Content>
+        </Card>
     )
 }
