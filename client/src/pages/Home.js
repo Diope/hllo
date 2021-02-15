ebeff3bfc8af72e03b80148103b7aa8c28a4d7f9
Dion Pettaway <dionp@fullsail.edu>
2021-02-15 16:59:45 -0500

    Setting up home page to iterate through posts

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import {useQuery} from '@apollo/client/react/hooks';
+import gql from 'graphql-tag'
 
 export default function Home() {
+    const {loading, data: {getPosts: posts}} = useQuery(FETCH_POSTS_QUERY);
+
+    if (data) {
+        console.log(data);
+    }
     return (
         <div>
             Home
         </div>
     )
 }
+
+const FETCH_POSTS_QUERY = gql`
+{
+    getPosts{
+        id body createdAt username likeCount
+        likes {
+            username
+        }
+        commentCount
+        comments {
+            id username createdAt body
+        }
+    }
+}
+`
